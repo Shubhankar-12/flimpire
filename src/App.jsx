47ea7769d8866b8b9d19a36ad9ee5c12ca214184
Shubhankar-12 <shubh.shubhankar2020@vitbhopal.ac.in>
2023-03-10 21:57:35 +0530

    comp added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { CssBaseline } from "@mui/material";
-import { Route, Routes } from "react-router";
-
+import { Routes, Route } from "react-router-dom";
 const App = () => {
   return (
     <div>
