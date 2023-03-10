afbdac653d79bba838eba715ced89acf4c7c3738
Shubhankar-12 <shubh.shubhankar2020@vitbhopal.ac.in>
2023-03-10 21:50:53 +0530

    init files

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { CssBaseline } from "@mui/material";
+import { Route, Routes } from "react-router";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <CssBaseline />
+      <main>
+        <Routes>
+          <Route exact path='/'>
+            <h1>Home</h1>
+          </Route>
+        </Routes>
+      </main>
+    </div>
+  );
 };
 
 export default App;
