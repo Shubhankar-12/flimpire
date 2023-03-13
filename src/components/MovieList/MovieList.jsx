ec356937b8ef6946ec3627e2c2e77bbd7337129d
Shubhankar-12 <shubh.shubhankar2020@vitbhopal.ac.in>
2023-03-13 20:49:33 +0530

    movie

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { Grid } from "@mui/material";
+import useStyles from "./moviesList.styles";
 
-const MovieList = () => {
-  return <div></div>;
+const MovieList = ({ movies }) => {
+  const classes = useStyles();
+  return (
+    <Grid container className={classes.moviesContainer}>
+      {movies.map((movie, i) => {
+        <Movie key={i} movie={movie} i={i} />;
+      })}
+    </Grid>
+  );
 };
 
 export default MovieList;
