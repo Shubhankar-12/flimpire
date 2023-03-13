9f33208795774f4f55cef302eeab3cb906213219
Shubhankar-12 <shubh.shubhankar2020@vitbhopal.ac.in>
2023-03-13 22:35:19 +0530

    movie

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { Grid } from "@mui/material";
 import useStyles from "./moviesList.styles";
+import { Movie } from "..";
 
 const MovieList = ({ movies }) => {
   const classes = useStyles();
   return (
     <Grid container className={classes.moviesContainer}>
-      {movies.map((movie, i) => {
-        <Movie key={i} movie={movie} i={i} />;
-      })}
+      {movies.results.map((movie, i) => (
+        <Movie key={i} movie={movie} i={i} />
+      ))}
     </Grid>
   );
 };
--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -7,11 +7,34 @@ import {
 } from "@mui/material";
 import { useSelector } from "react-redux";
 import { useGetMoviesQuery } from "../../services/TMDB";
+import { MovieList } from "..";
 
 const Movies = () => {
-  const { data } = useGetMoviesQuery();
-  console.log(data);
-  return <div>Movies</div>;
+  const { data, error, isFetching } = useGetMoviesQuery();
+  if (isFetching) {
+    return (
+      <Box justifyContent='center' display='flex'>
+        <CircularProgress size='4rem' />
+      </Box>
+    );
+  }
+  if (!data.results.length) {
+    return (
+      <Box display='flex' alignItems='center' mt='20px'>
+        <Typography variant='h4'>
+          No Movies that match that name.
+          <br />
+          Please search for something else.
+        </Typography>
+      </Box>
+    );
+  }
+  if (error) return "An error has occured.";
+  return (
+    <div>
+      <MovieList movies={data} />
+    </div>
+  );
 };
 
 export default Movies;
