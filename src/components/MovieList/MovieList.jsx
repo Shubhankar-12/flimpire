bab4d9e5490db5c934b95d15bb25d92be27083a1
Shubhankar-12 <shubh.shubhankar2020@vitbhopal.ac.in>
2023-03-19 22:30:51 +0530

    sliced view for recommendation

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -3,11 +3,11 @@ import { Grid } from "@mui/material";
 import useStyles from "./moviesList.styles";
 import { Movie } from "..";
 
-const MovieList = ({ movies }) => {
+const MovieList = ({ movies, numOfMovies }) => {
   const classes = useStyles();
   return (
     <Grid container className={classes.moviesContainer}>
-      {movies.results.map((movie, i) => (
+      {movies.results.slice(0, numOfMovies).map((movie, i) => (
         <Movie key={i} movie={movie} i={i} />
       ))}
     </Grid>
