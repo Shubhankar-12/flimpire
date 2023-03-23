534725aeff705bddee8cabc12509fade725a8ae3
Shubhankar-12 <shubh.shubhankar2020@vitbhopal.ac.in>
2023-03-23 22:28:04 +0530

    responsive

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,19 +1,30 @@
 import React, { useState, useEffect } from "react";
-import { Box, CircularProgress, Typography } from "@mui/material";
+import {
+  Box,
+  CircularProgress,
+  Typography,
+  useMediaQuery,
+} from "@mui/material";
 import { useSelector } from "react-redux";
 import { useGetMoviesQuery } from "../../services/TMDB";
 import { MovieList, Pagination } from "..";
 
 const Movies = () => {
   const [page, setPage] = useState(1);
+
   const { genreIdOrCategoryName, searchQuery } = useSelector(
     (state) => state.currentGenreOrCategory
   );
+
   const { data, error, isFetching } = useGetMoviesQuery({
     genreIdOrCategoryName,
     page,
     searchQuery,
   });
+
+  const lg = useMediaQuery((theme) => theme.breakpoints.only("lg"));
+  const numberOfMovies = lg ? 17 : 19;
+
   if (isFetching) {
     return (
       <Box justifyContent='center' display='flex'>
@@ -35,7 +46,7 @@ const Movies = () => {
   if (error) return "An error has occured.";
   return (
     <div>
-      <MovieList movies={data} />
+      <MovieList movies={data} numOfMovies={numberOfMovies} />
       <Pagination
         currentPage={page}
         setPage={setPage}
