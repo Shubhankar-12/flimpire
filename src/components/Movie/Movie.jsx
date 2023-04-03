93b943ce0029fb400febc2bb1e185498d99a2b2b
Shubhankar-12 <shubh.shubhankar2020@vitbhopal.ac.in>
2023-04-03 22:39:53 +0530

    box shadow better now

--- src/components/Movie/Movie.jsx
+++ src/components/Movie/Movie.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Typography, Grid, Grow, Tooltip, Rating } from "@mui/material";
 import { Link } from "react-router-dom";
 import useStyles from "./Movie.Styles";
