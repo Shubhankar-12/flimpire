ea28fc018dfe96c532a043954e66c5f1df559d72
Shubhankar-12 <shubh.shubhankar2020@vitbhopal.ac.in>
2023-04-12 23:27:00 +0530

    alan key update

--- src/components/Alan.jsx
+++ src/components/Alan.jsx
@@ -1,11 +1,12 @@
 import alanBtn from "@alan-ai/alan-sdk-web";
 import { useContext, useEffect } from "react";
 import { ColorModeContext } from "../utils/ToggleColorMode";
+const alanKey = process.env.REACT_APP_ALAN_KEY;
 const useAlan = () => {
   const { setMode } = useContext(ColorModeContext);
   useEffect(() => {
     alanBtn({
-      key: "f9e9a90a49207b7c8145d45cad5cb5ee2e956eca572e1d8b807a3e2338fdd0dc/stage",
+      key: alanKey,
 
       onCommand: ({ command, mode }) => {
         if (command === "changeMode") {
