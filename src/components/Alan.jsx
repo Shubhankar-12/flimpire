767f2d5a60b3dc52523e7b1da957d9edb8f5bb5a
Shubhankar-12 <shubh.shubhankar2020@vitbhopal.ac.in>
2023-04-12 23:10:35 +0530

    shown on the app

--- src/components/Alan.jsx
+++ src/components/Alan.jsx
@@ -5,11 +5,15 @@ const useAlan = () => {
   const { setMode } = useContext(ColorModeContext);
   useEffect(() => {
     alanBtn({
-      key: process.env.REACT_APP_ALAN_KEY,
+      key: "f9e9a90a49207b7c8145d45cad5cb5ee2e956eca572e1d8b807a3e2338fdd0dc/stage",
+
       onCommand: ({ command, mode }) => {
         if (command === "changeMode") {
-          if (mode === "light") setMode("dark");
-          else setMode("light");
+          if (mode === "light") {
+            setMode("dark");
+          } else {
+            setMode("light");
+          }
         }
       },
     });
