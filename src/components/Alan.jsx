4c584660a16d2c93031468851cab5de3228c73d2
Shubhankar-12 <shubh.shubhankar2020@vitbhopal.ac.in>
2023-04-12 23:31:00 +0530

    login logout

--- src/components/Alan.jsx
+++ src/components/Alan.jsx
@@ -1,9 +1,12 @@
 import alanBtn from "@alan-ai/alan-sdk-web";
 import { useContext, useEffect } from "react";
 import { ColorModeContext } from "../utils/ToggleColorMode";
+import { fetchToken } from "../utils/index.util";
+import { useNavigate } from "react-router-dom";
 const alanKey = process.env.REACT_APP_ALAN_KEY;
 const useAlan = () => {
   const { setMode } = useContext(ColorModeContext);
+  const navigate = useNavigate();
   useEffect(() => {
     alanBtn({
       key: alanKey,
@@ -15,6 +18,11 @@ const useAlan = () => {
           } else {
             setMode("light");
           }
+        } else if (command === "login") {
+          fetchToken();
+        } else if (command === "logout") {
+          localStorage.clear();
+          navigate("/");
         }
       },
     });
