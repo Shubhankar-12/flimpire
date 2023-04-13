b938f880cd09644c20f9ce7ae5bf14fc28c630a5
Shubhankar-12 <shubh.shubhankar2020@vitbhopal.ac.in>
2023-04-13 22:15:34 +0530

    search alan

--- src/components/Alan.jsx
+++ src/components/Alan.jsx
@@ -3,16 +3,36 @@ import { useContext, useEffect } from "react";
 import { ColorModeContext } from "../utils/ToggleColorMode";
 import { fetchToken } from "../utils/index.util";
 import { useNavigate } from "react-router-dom";
+import {
+  searchMovie,
+  selectGenreOrCategory,
+} from "../features/currentGenreOrCategory";
+import { useDispatch } from "react-redux";
 const alanKey = process.env.REACT_APP_ALAN_KEY;
 const useAlan = () => {
   const { setMode } = useContext(ColorModeContext);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   useEffect(() => {
     alanBtn({
       key: alanKey,
 
-      onCommand: ({ command, mode }) => {
-        if (command === "changeMode") {
+      onCommand: ({ command, mode, genres, genreOrCategory, query }) => {
+        if (command === "chooseGenre") {
+          const foundGenre = genres.find(
+            (g) => g.name.toLowerCase() === genreOrCategory.toLowerCase()
+          );
+          if (foundGenre) {
+            navigate("/");
+            dispatch(selectGenreOrCategory(foundGenre.id));
+          } else {
+            const category = genreOrCategory.startsWith("top")
+              ? "top_rated"
+              : genreOrCategory;
+            navigate("/");
+            dispatch(selectGenreOrCategory(category));
+          }
+        } else if (command === "changeMode") {
           if (mode === "light") {
             setMode("dark");
           } else {
@@ -23,6 +43,8 @@ const useAlan = () => {
         } else if (command === "logout") {
           localStorage.clear();
           navigate("/");
+        } else if (command === "search") {
+          dispatch(searchMovie(query));
         }
       },
     });
