c27962aa1b576c4f4b67618537bf3779f84b0f89
Shubhankar-12 <shubh.shubhankar2020@vitbhopal.ac.in>
2023-04-14 21:49:35 +0530

    minor changes

--- src/components/MovieList/moviesList.styles.js
+++ src/components/MovieList/moviesList.styles.js
@@ -4,7 +4,7 @@ export default makeStyles((theme) => ({
   moviesContainer: {
     display: "flex",
     flexWrap: "wrap",
-    justifyContent: "center",
+    justifyContent: "space-between",
     overflow: "auto",
     [theme.breakpoints.down("sm")]: {
       justifyContent: "center",
