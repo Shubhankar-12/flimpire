6399ed7a8fa2a749144351ae263d13d53849fb79
Shubhankar-12 <shubh.shubhankar2020@vitbhopal.ac.in>
2023-04-14 21:49:18 +0530

    minor changes

--- src/components/Movie/Movie.jsx
+++ src/components/Movie/Movie.jsx
@@ -6,7 +6,7 @@ const Movie = ({ movie, i }) => {
   const classes = useStyles();
   return (
     <Grid item xs={12} sm={6} md={4} lg={3} xl={2} className={classes.movie}>
-      <Grow in={Boolean(i)} timeout={(i + 1) * 250}>
+      <Grow in key={i} timeout={(i + 1) * 250}>
         <Link className={classes.links} to={`/movie/${movie.id}`}>
           <img
             src={
